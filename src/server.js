e4fa63e1267f727f21d9f56439e0e5dd1e518fca
hakisolos <maxwellexcel174@gmail.com>
2025-02-24 12:27:06 -0800

    GOD DID

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ let chatHistory = [];
 let facts = {};
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public/")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
