671707fab740adf74d1b917c6d9d14db1dc482f6
Iron Man <narendrannirmal6753@gmail.com>
2025-02-25 19:05:51 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -63,6 +63,13 @@ app.post("/api", async (req, res) => {
   }
 });
 
+var start = async () => {
+  try {
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+start();
