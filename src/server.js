41d185fbd1d1fb71216bd4f5efbe3a6cb7128990
Iron Man <narendrannirmal6753@gmail.com>
2025-02-25 18:41:43 +0530

    Lalalala

--- src/server.js
+++ src/server.js
@@ -13,9 +13,8 @@ app.use(express.static(path.join(__dirname, "../public/")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
-
 app.get("/ai", (req, res) => {
-  res.sendFile("ai.html", { root: path.join(__dirname, "../public") });
+  res.sendFile(path.join(__dirname, "../public", "ai.html"));
 });
 
 
